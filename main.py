92bc8de351564de700be47380378dfd4d4801ee0
jordanm020304 <jordanm020304@gmail.com>
2023-03-21 12:01:25 -0400

    commit j

--- main.py
+++ main.py
@@ -6,23 +6,25 @@ def encoder(value):
         passcode += encoded_digit
     return passcode
 
-def decoder(value):
-    passcode = ''
-    for digit in value:
-        decoded_digit = str((int(digit) - 3) % 10)
-        passcode += decoded_digit
-    return passcode
+def decode(value):
+    password = ''
+    for num in value:
+        decoded_digit = str((int(num) - 3) % 10)
+        password += decoded_digit
+    return password
 
 def main():
     run = True
     while run:
-        print("1. To encode /n 2. To decode /n 3. To exit")
+        print("1. To encode \n 2. To decode \n 3. To exit")
         choice = input("Enter your choice (1 or 2 or 3): ")
         if choice == "1":
             digits = input("Enter an 8-digit password: ")
             print("Your password has been encoded and stored!")
+
+
         elif choice == "2":
-            print(f'The encoded password is {encoder(digits)}, and the original password is {decoder(digits)}')
+            print(f'The encoded password is {encoder(digits)}, and the original password is {digits}')
         elif choice == "3":
             break
         else:
